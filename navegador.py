5f8b05598fd32293b98f6224c2afb2aab5194374
Arthur A <aires.arthur.15@gmail.com>
2021-03-15 19:50:30 -0300

    add regex url validator

--- navegador.py
+++ navegador.py
@@ -1,5 +1,5 @@
 import requests
-
+import re
 
 class Navegador:
 
@@ -21,3 +21,13 @@ class Navegador:
 		else:
 
 			return self.sessionRequest.get(url)
+	
+	def urlValidator(self, url):
+		regex = re.compile(
+    r'^(?:http|ftp)s?://' # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain...
+    r'localhost|' # localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' # ...or ipv4
+    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' # ...or ipv6
+    r'(?::\d+)?' # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
