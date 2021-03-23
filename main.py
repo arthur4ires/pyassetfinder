2e4eb033f1f9a5297e9ebf8dcb7dfc7ec47d44e9
Arthur A <aires.arthur.15@gmail.com>
2021-03-23 00:11:18 -0300

    add status_code and dev_mode

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import fontes.subdomainfinder
 import sys
 import socket
 import argparse
+import navegador
 
 domainList = []
 
@@ -37,7 +38,9 @@ def argParserCommands():
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-u','--url', dest="domainName", help='Você deve setar o domínio a ser pesquisado!', required=True)
-	parser.add_argument('--dev-mode', dest="debugName", help='APIs em desenvolvimento!', default=False)
+	parser.add_argument('--status-code', dest="statusCode", help='Verifica se exister um servidor web e se ele está respondendo!', default=False, action="store_true")
+	parser.add_argument('--ip-block', dest="ipBlock", help='Não exibe domínios com um IP específico!', default=False, action="store_true")
+	parser.add_argument('--dev-mode', dest="debugName", help='APIs em desenvolvimento!', default=False, action="store_true")
 
 	return parser.parse_args()
 
@@ -45,32 +48,34 @@ if __name__ == "__main__":
 	
 	returnComands = argParserCommands()
 
-	if returnComands.domainName != "":
 
-		if returnComands.debugName == False:
+	if returnComands.debugName == False:
 
-			printDomainList(fontes.hackertarget.returnDomains(returnComands.domainName))
-			printDomainList(fontes.ctrsh.returnDomains(returnComands.domainName))
-			printDomainList(fontes.certspotter.returnDomains(returnComands.domainName))
-			printDomainList(fontes.bufferoverun.returnDomains(returnComands.domainName))
-			printDomainList(fontes.threatcrowd.returnDomains(returnComands.domainName))
-
-		else:
-			fontes.subdomainfinder.returnDomains(returnComands.domainName)
-		
-		for domainAndIp in domainList:
-
-			ipDomain =  getIpFromHostName(domainAndIp)
-
-			try:
-				ipBlocked = sys.argv[3]
-
-				if ipBlocked != ipDomain:
-					print(domainAndIp, ipDomain)
-
-			except:
-				print(domainAndIp,ipDomain)
+		printDomainList(fontes.hackertarget.returnDomains(returnComands.domainName))
+		printDomainList(fontes.ctrsh.returnDomains(returnComands.domainName))
+		printDomainList(fontes.certspotter.returnDomains(returnComands.domainName))
+		printDomainList(fontes.bufferoverun.returnDomains(returnComands.domainName))
+		printDomainList(fontes.threatcrowd.returnDomains(returnComands.domainName))
 
 	else:
 
-		print("[+] Você deve setar o domínio!")
+		fontes.subdomainfinder.returnDomains(returnComands.domainName)
+	
+	for domainAndIp in domainList:
+
+		ipDomain =  getIpFromHostName(domainAndIp)
+		
+		if returnComands.ipBlock != False and returnComands.ipBlock == ipDomain:
+			continue
+
+		domainReturn = domainAndIp
+
+		if returnComands.statusCode != False:
+
+			statusCode = navegador.Navegador().downloadResponse('http://{}'.format(domainAndIp),'STATUS','GET').status_code
+
+			domainReturn += ' - ({})'.format(statusCode)
+		
+		domainReturn += ' {}'.format(ipDomain)
+
+		print(domainReturn)
